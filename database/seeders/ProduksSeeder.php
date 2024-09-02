1b8c75773873d647ce0596631bd054ab71eb1427
WIN-6RJT1SUOGOG\Administrator <daffakudo123@gmail.com>
2024-09-02 12:49:47 +0700

    edit produksseeder

--- database/seeders/ProduksSeeder.php
+++ database/seeders/ProduksSeeder.php
@@ -25,7 +25,7 @@ class ProduksSeeder extends Seeder
             ['id' => '8', 'nama' => 'Tareng', 'jenis' => 'Makanan', 'harga' => '5000', 'foto' => 'produk/img8.png', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'kantin_id' => '1', 'trending' => false],
             ['id' => '9', 'nama' => 'Chicken Crispy', 'jenis' => 'Makanan', 'harga' => '7500', 'foto' => 'produk/img9.png', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'kantin_id' => '1', 'trending' => false],
             ['id' => '10', 'nama' => 'Basreng', 'jenis' => 'Makanan', 'harga' => '5000', 'foto' => 'produk/img10.png', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'kantin_id' => '1', 'trending' => false],
-            ['id' => '11', 'nama' => 'Nasi Ayam', 'jenis' => 'Makanan', 'harga' => '12000', 'foto' => 'produk/img11.png', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'kantin_id' => '1', 'trending' => false],
+            ['id' => '11', 'nama' => 'Paket Ayam', 'jenis' => 'Makanan', 'harga' => '12000', 'foto' => 'produk/img11.png', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'kantin_id' => '1', 'trending' => false],
             ['id' => '12', 'nama' => 'Mie Ayam', 'jenis' => 'Makanan', 'harga' => '8000', 'foto' => 'produk/img12.png', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'kantin_id' => '2', 'trending' => false],
             ['id' => '13', 'nama' => 'Udon', 'jenis' => 'Makanan', 'harga' => '7000', 'foto' => 'produk/img13.png', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'kantin_id' => '1', 'trending' => false],
         ];
